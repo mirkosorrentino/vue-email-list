cb1848b70b974a79aff5062b0a6de9097a5e1515
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-04-16 12:27:29 +0200

    Job almost done but in need of hotwell ticket

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+
+const { createApp } = Vue;
+
+createApp ({
+    data(){
+        return{
+            emails: []
+        };
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((resp) => {
+            const email = resp.data.responnse;
+            this.emails.push(email);
+            });
+        }
+    },
+}).mount("#app");
+
 /** API
 https://flynn.boolean.careers/exercises/api/random/mail
 */
